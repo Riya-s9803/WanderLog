b9cf3fe4443b7ec05109026f824d7296541b86f5
Riya-s9803 <riyasingh09082003@gmail.com>
2025-07-02 23:36:03 +0530

    list of country,storing state in url,navigation

--- WanderLog/src/components/Form.jsx
+++ WanderLog/src/components/Form.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import styles from "./Form.module.css";
+import { useNavigate } from "react-router-dom";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -13,6 +14,8 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
+  const navigate=useNavigate();
+
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
@@ -49,8 +52,11 @@ function Form() {
       </div>
 
       <div className={styles.buttons}>
-        <button>Add</button>
-        <button>&larr; Back</button>
+        <button type="primary">Add</button>
+        <button type="back" onClick={(e)=>{
+          e.preventDefault();
+          navigate(-1);
+        }}>&larr; Back</button>
       </div>
     </form>
   );
